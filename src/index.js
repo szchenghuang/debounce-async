d0e80bb800b05b6c854bc8a2afd639c5cc63c46d
szchenghuang <szchenghuang@gmail.com>
2018-01-11 21:38:50 +0800

    Rename variables.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,38 @@
-function debounce( func, wait = 0, { leading = false, cancelObj = 'canceled' } = {} ) {
-  let timer, latestResolve, shouldCancel
+function debounce(
+  func,
+  wait = 0,
+  {
+    leading = false,
+    cancelObj = 'canceled'
+  } = {}
+) {
+  let timerId, latestResolve, shouldCancel
 
   return function ( ...args ) {
     if ( !latestResolve ) {
       return leading ?
         func.apply( this, args ) : new Promise( ( resolve, reject ) => {
           latestResolve = resolve
-          timer = setTimeout( exec.bind( this, args, resolve, reject ), wait )
-        });
+          timerId = setTimeout( invokeFunc.bind( this, args, resolve, reject ), wait )
+        })
     }
 
     shouldCancel = true;
     return new Promise( ( resolve, reject ) => {
       latestResolve = resolve
-      timer = setTimeout( exec.bind( this, args, resolve, reject ), wait )
+      timerId = setTimeout( invokeFunc.bind( this, args, resolve, reject ), wait )
     })
   }
 
-  function exec( args, resolve, reject ) {
+  function invokeFunc( args, resolve, reject ) {
     if ( shouldCancel && resolve !== latestResolve ) {
       reject( cancelObj )
     } else {
       func.apply( this, args ).then( resolve ).catch( reject )
       if ( resolve === latestResolve ) {
         shouldCancel = false
-        clearTimeout( timer )
-        timer = latestResolve = null
+        clearTimeout( timerId )
+        timerId = latestResolve = null
       }
     }
   }
